9a1a07403b9354367f86844b61060ff81c71a771
Bakso14 <70924870+Bakso14@users.noreply.github.com>
2026-01-24 15:20:58 +0700

    bismilliah_fix

--- data/penelitians.php
+++ data/penelitians.php
@@ -12,6 +12,20 @@ return [
 4. Memiliki minat pada bidang energi terbarukan atau sistem monitoring',
         'kuota' => 2,
         'link' => "https://wa.me/+628113300303",
+        'deadline' => "-",
+        ],
+
+        ['judul' => 'Optimasi Pemanfaatan Energi Terbarukan pada Sistem Pompa Air Hybrid PV–PLN', 
+        'tahun' => 2026,
+        'deskripsi' => 'Deskripsi: Penelitian ini berfokus pada perancangan dan implementasi sistem monitoring serta optimasi pemanfaatan energi terbarukan pada sistem pompa air dengan konfigurasi hybrid panel surya (PV) dan jaringan listrik PLN. Panel surya digunakan sebagai sumber energi utama, sementara PLN berfungsi sebagai sumber cadangan untuk menjaga keandalan suplai air. Sistem tandon air dirancang untuk mengoptimalkan pengaturan waktu operasi pompa. Optimasi dilakukan dengan mengatur pola pengisian tandon dan pemilihan sumber energi berdasarkan kondisi operasional, dengan tujuan memaksimalkan kontribusi energi terbarukan  dengan tetap menjaga ketersediaan air.
+        
+        <strong>Requirements</strong>:
+1. Memiliki keahlian atau kemauan untuk belajar di bidang sistem energi terbarukan (panel surya, sistem hybrid PV–PLN)
+2. Memiliki keahlian atau kemauan untuk belajar di bidang desain dan integrasi hardware (sensor, mikrokontroler, sistem pompa)
+3. Memiliki kemampuan programming Python / Arduino / C++',
+        'kuota' => 2,
+        'link' => "https://wa.me/+628113300303",
+        'deadline' => "-",
         ],
 
     ],
@@ -21,12 +35,13 @@ return [
         'deskripsi' => 'StickandSee merupakan tongkat tuna netra yang telah dikembangkan dari tahun 2024-2025 dengan tujuan untuk memberikan kemudahan dan kenyamanan bagi tuna netra saat berjalan. StickandSee menggunakan mikrokontroler berbasis XIAO ESP32 sebagai perangkat untuk implementasi kecerdasan buatan khususnya deteksi objek seperti mobil atau motor yang berada di hadapan tuna netra dengan menggunakan onboard camera OV2640. StickandSee juga memiliki fitur lain seperti GPS sebagai lokalisasi posisi user, sensor proximity E18-D80NK untuk mendeteksi adanya lubang yang dalam di jalan atau trotoar, sensor ultrasonic SR04 untuk mendeteksi adanya obstacle di depan user.
         
         <strong>Requirements</strong>:
-1. Memiliki keahlian/kemauan untuk belajar di bidang desain 3D
-2. Memiliki keahlian/kemauan untuk belajar di bidang desain hardware
+1. Memiliki keahlian di bidang desain 3D
+2. Memiliki keahlian di bidang desain hardware
 3. Memiliki kemampuan programming Python/Arduino/C++
 ',
         'kuota' => 1,
         'link' => "https://forms.gle/17K7cRf9UNM3CAidA",
+        'deadline' => "29 Januari 2026",
         ],
 
         ['judul' => 'ImHere: Perancangan Aplikasi Monitoring Tongkat Tuna Netra "StickandSee" berbasis IoT untuk Mengetahu Posisi dan Lokasi Orang dengan Keterbatasan Visual', 
@@ -34,12 +49,13 @@ return [
         'deskripsi' => '"ImHere"  merupakan aplikasi berbasis Android/iOS sebagai salah satu paket bawaan dari tongkat tuna netra "StickandSee" yang dapat digunakan oleh kerabat atau orang terdekat dari orang dengan keterbatasan visual yang dirancang untuk memonitor pergerakan atau lokasi dari user "StickandSee" dan memberikan informasi terkini terkait kondisi user.
         
         <strong>Requirements</strong>:
-1. Memiliki keahlian/kemauan untuk belajar di bidang desain 3D
-2. Memiliki keahlian/kemauan untuk belajar di bidang desain hardware
+1. Memiliki keahlian di bidang desain 3D
+2. Memiliki keahlian di bidang desain hardware
 3. Memiliki kemampuan programming Python/Arduino/C++
 ',
         'kuota' => 1,
         'link' => "https://forms.gle/17K7cRf9UNM3CAidA",
+        'deadline' => "29 Januari 2026",
         ],
 
         ['judul' => 'Sistem Odometri Mobile Robot berbasis PID sebagai Robot Assistant di Restoran untuk Proses Pengantaran Makanan', 
@@ -47,12 +63,13 @@ return [
         'deskripsi' => 'Mobile robot yang akan dikembangkan adalah mobile robot dengan menggunakan mekanisme gerak differential drive 2 roda. Odometri merupakan salah satu metode lokalisasi robot untuk mengetahui arah gerak robot dan posisi robot berdasarkan gerakan roda robot. PID controller digunakan sebagai metode kendali agar robot dapat bergerak ke posisi yang dituju dengan akurat. Dengan adanya sistem odometri yang akurat diharapkan dapat membantu robot dalam berjalan menuju posisi tertentu untuk proses pengantaran makanan ke meja makan customer di dalam sebuah restoran 
         
         <strong>Requirements</strong>:
-1. Memiliki keahlian/kemauan untuk belajar di bidang desain 3D
-2. Memiliki keahlian/kemauan untuk belajar di bidang desain hardware
+1. Memiliki keahlian di bidang desain 3D
+2. Memiliki keahlian di bidang desain hardware
 3. Memiliki kemampuan programming Python/Arduino/C++
 ',
         'kuota' => 1,
         'link' => "https://forms.gle/17K7cRf9UNM3CAidA",
+        'deadline' => "29 Januari 2026",
         ],
 
     ],
@@ -62,6 +79,7 @@ return [
         'deskripsi' => 'Terdapat banyak sekali area terbatas bagi manusia, sebagian besar karena resiko kesehatan. Penelitian ini diajukan sebagai sebuah solusi dalam inspeksi infrastuktur beresiko pada area terbatas dengan memanfaatkan robot omniwheel yang dapat bergerak bebas ke segala arah. Area riset tahun ini pada path planning robot yang dimana menentukan seberapa efisien gerak robot dari satu titik menuju titik tujuan. Area terbatas yang akan menjadi lokasi uji coba adalah pabrik air kemasan yang dalam proses produksinya banyak sekali menghasilkan ozon yang berbahaya bagi pernafasan dan mata manusia.',
         'kuota' => 1,
         'link' => "",
+        'deadline' => "27 Januari 2026",
         ],
 
          ['judul' => 'Autonomous Car Sebagai Solusi Otomatisasi Pemindahan Logistik Barang: Prototyoe', 
@@ -72,16 +90,7 @@ return [
         2. Pendeteksian objek sekitar mobil dengan memanfaatkan lidar',
         'kuota' => 2,
         'link' => "",
-        ],
-
-        ['judul' => 'Autonomous Car Sebagai Solusi Otomatisasi Pemindahan Logistik Barang: Prototyoe', 
-        'tahun' => 2026,
-        'deskripsi' => 'Autonomous Car merupakan teknologi yang kompleks karena terdiri dari berbagai jenis sensor dan harus saling bersinergi dalam mendeteksi apapun yang terjadi disekitarnya. Penelitian ini berfokus pada proses pendeteksian objek sekitar mobil otonom
-        Ada dua topik yang diriset pada penelitian ini:
-        1. Pendeteksian jalur berkendara aman pada mobil otonom berdasarkan struktur dan marka jalan
-        2. Pendeteksian objek sekitar mobil dengan memanfaatkan lidar',
-        'kuota' => 2,
-        'link' => "",
+        'deadline' => "27 Januari 2026",
         ],
 
     ],
@@ -117,6 +126,7 @@ return [
 3. Familiar dengan aplikasi flutter/ MIT app Inventor/Android Studio',
         'kuota' => 2,
         'link' => "https://unesa.me/MagangRisetYandhika",
+        'deadline' => "-",
         ],
         
     ],
